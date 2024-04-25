1061aee84261a816087efdfab1717327abfb6691
Ledi Li <ledili2424@gmail.com>
2024-04-25 16:56:04 -0700

    install dotenv

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRouter = require("./routes/authRoutes.js");
 const passwordRouter = require("./routes/passwordRoutes.js");
-const path = require("path");
+
 
 dotenv.config({ path: "./config.env" });
 
@@ -18,13 +18,6 @@ mongoose
   .connect(process.env.DATABASE)
   .then(() => console.log("connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
-
-// let frontendDir = path.join(__dirname, "../frontend/dist");
-// app.use(express.static(frontendDir));
-// app.get("*", function (req, res) {
-//   console.log("Received request");
-//   res.sendFile(path.join(frontendDir, "index.html"));
-// });
 
 app.use("/api/auth", authRouter);
 app.use("/api/password", passwordRouter);
