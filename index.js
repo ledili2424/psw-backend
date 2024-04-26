8539d398d0290edc0db506f79d0a73741a5c4949
Ledi Li <ledili2424@gmail.com>
2024-04-25 17:17:17 -0700

    change cors url to deploy url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config({ path: "./config.env" });
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://ledili-project3.onrender.com", credentials: true }));
 app.use(express.json());
 
 mongoose
